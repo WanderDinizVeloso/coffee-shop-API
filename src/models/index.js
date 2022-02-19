f81faed2940fa01c270810d5536840a55042eda8
WanderDinizVeloso <wanveloso@yahoo.com.br>
2022-02-19 10:31:29 -0300

    refatora models visando unificar as buscas em uma unica funçao e implementando aggregate para tal finalidade

--- src/models/index.js
+++ src/models/index.js
@@ -1,19 +1,11 @@
 const create = require('./documents/create');
 const remove = require('./documents/remove');
-const searchAll = require('./documents/searchAll');
-const searchById = require('./documents/searchById');
-const searchByField = require('./documents/searchByField');
+const search = require('./documents/search');
 const update = require('./documents/update');
 
 module.exports = (collection) => ({
   create: async (doc) => create(collection, doc),
   remove: async (id) => remove(collection, id),
-  searchAll: async (filterConfig, attributesConfig, sortConfig) => searchAll(
-    collection, filterConfig, attributesConfig, sortConfig,
-  ),
-  searchByField: async (field, attributesConfig) => searchByField(
-    collection, field, attributesConfig,
-  ),
-  searchById: async (id, attributesConfig) => searchById(collection, id, attributesConfig),
+  search: async (pipeline) => search(collection, pipeline),
   update: async (doc) => update(collection, doc),
 });
