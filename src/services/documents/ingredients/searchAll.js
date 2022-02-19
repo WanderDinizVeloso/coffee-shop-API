89f8747f2355fc2aaac61a0aed152737a3b331d5
WanderDinizVeloso <wanveloso@yahoo.com.br>
2022-02-19 20:11:05 -0300

    corrige erro de parametro do search do services ingredients searchAll

--- src/services/documents/ingredients/searchAll.js
+++ src/services/documents/ingredients/searchAll.js
@@ -2,7 +2,7 @@ const { INGREDIENTS } = require('../../../utils/strings');
 const { search } = require('../../../models')(INGREDIENTS);
 
 module.exports = async () => {
-  const ingredients = await search({});
+  const ingredients = await search();
 
   return ingredients;
 };
