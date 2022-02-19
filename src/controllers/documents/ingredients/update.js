9b5d06b7a3c33bfefa7fd168de640a9b6c38dcff
WanderDinizVeloso <wanveloso@yahoo.com.br>
2022-02-19 19:31:50 -0300

    cria controllers ingredients update

---
+++ src/controllers/documents/ingredients/update.js
@@ -0,0 +1,27 @@
+const { OK } = require('http-status-codes').StatusCodes;
+
+const { update } = require('../../../services/documents/ingredients');
+const { notFound, registered, modifiedSuccessfully } = require('../../statusAndMessage');
+const { INGREDIENT, NAME_EXIST, NEW_NAME } = require('../../../utils/strings');
+
+module.exports = async (req, res, next) => {
+  const { id } = req.params;
+  const { name, unitary, price } = req.body;
+
+  const updated = await update({ id, name, unitary, price });
+
+  if (!updated) {
+    return next(notFound(INGREDIENT));
+  }
+
+  if (updated === NAME_EXIST) {
+    return next(registered(NEW_NAME));
+  }
+
+  return res
+    .status(OK)
+    .json({
+      message: modifiedSuccessfully(INGREDIENT),
+      updatedIngredient: updated,
+    });
+};
