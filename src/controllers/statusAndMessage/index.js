dae8eca793a52ce864b2d03da2b8ab2b13d638d3
WanderDinizVeloso <wanveloso@yahoo.com.br>
2022-02-18 22:03:19 -0300

    cria unauthorized no controllers statusAndMessage

--- src/controllers/statusAndMessage/index.js
+++ src/controllers/statusAndMessage/index.js
@@ -1,4 +1,4 @@
-const { BAD_REQUEST, NOT_FOUND } = require('http-status-codes').StatusCodes;
+const { BAD_REQUEST, NOT_FOUND, UNAUTHORIZED } = require('http-status-codes').StatusCodes;
 
 const internalError = () =>
   'sorry, internal error.';
@@ -13,8 +13,14 @@ const notFound = (param) => ({
   message: `'${param}' not found.`,
 });
 
+const unauthorized = () => ({
+  status: UNAUTHORIZED,
+  message: 'Request not allowed for this user.',
+});
+
 module.exports = {
   internalError,
   invalid,
   notFound,
+  unauthorized,
 };
