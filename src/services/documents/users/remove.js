69e75f0b766140a4ed905efb1839657bc2662836
WanderDinizVeloso <wanveloso@yahoo.com.br>
2022-02-19 12:16:35 -0300

    implementa models search no services users remove

--- src/services/documents/users/remove.js
+++ src/services/documents/users/remove.js
@@ -1,10 +1,10 @@
 const { USERS, TOKENS, ADMIN } = require('../../../utils/strings');
-const { remove, searchById } = require('../../../models')(USERS);
+const { remove, search } = require('../../../models')(USERS);
 const { create } = require('../../../models')(TOKENS);
-const { ATTRIBUTE_DISABLED } = require('../../../utils/magicNumbers');
+const { filterField } = require('../../../utils/pipelines');
 
 module.exports = async ({ id, role, token }) => {
-  const user = await searchById(id, { password: ATTRIBUTE_DISABLED });
+  const user = (await search(filterField({ _id: id })))[0];
 
   if (!user) {
     return null;
