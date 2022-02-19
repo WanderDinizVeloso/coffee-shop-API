8ff35de9b943910dd7b35040c133b04775a0660d
WanderDinizVeloso <wanveloso@yahoo.com.br>
2022-02-19 15:32:00 -0300

    cria checkNewEmailOnDatabase no services functions

---
+++ src/services/functions/checkNewEmailOnDatabase.js
@@ -0,0 +1,16 @@
+const { USERS } = require('../../utils/strings');
+const { search } = require('../../models')(USERS);
+const { filterField } = require('../../utils/pipelines');
+
+module.exports = async (newEmail, emailData) => {
+  console.log(newEmail, emailData);
+  if (newEmail !== emailData) {
+    const verifiedEmail = (await search(filterField({ email: newEmail })))[0];
+
+    console.log(verifiedEmail);
+
+    return verifiedEmail;
+  }
+
+  return null;
+};
