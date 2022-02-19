a0ea51a95cd5c5cc240e35ad0161b4936c68ac2b
WanderDinizVeloso <wanveloso@yahoo.com.br>
2022-02-19 16:42:37 -0300

    implementa setDecimalPlaces no index do services functions

--- src/services/functions/index.js
+++ src/services/functions/index.js
@@ -1,7 +1,9 @@
 const stringInNumber = require('./stringInNumber');
 const checkNewEmailOnDatabase = require('./checkNewEmailOnDatabase');
+const setDecimalPlaces = require('./setDecimalPlaces');
 
 module.exports = {
   stringInNumber,
   checkNewEmailOnDatabase,
+  setDecimalPlaces,
 };
