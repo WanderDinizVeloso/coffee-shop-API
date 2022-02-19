97bbe0b0ac0390c068f0a25845ce33c47bf370f4
WanderDinizVeloso <wanveloso@yahoo.com.br>
2022-02-18 21:48:24 -0300

    cria invalid e notFound no controllers statusAndMessage

--- src/controllers/statusAndMessage/index.js
+++ src/controllers/statusAndMessage/index.js
@@ -1,6 +1,20 @@
+const { BAD_REQUEST, NOT_FOUND } = require('http-status-codes').StatusCodes;
+
 const internalError = () =>
   'sorry, internal error.';
 
+const invalid = (param) => ({
+  status: BAD_REQUEST,
+  message: `The invalid '${param}' field.`,
+});
+
+const notFound = (param) => ({
+  status: NOT_FOUND,
+  message: `'${param}' not found.`,
+});
+
 module.exports = {
   internalError,
+  invalid,
+  notFound,
 };
