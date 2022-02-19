c6e561c17112424e19aee09c3fa95435fd36d7ad
WanderDinizVeloso <wanveloso@yahoo.com.br>
2022-02-19 00:02:51 -0300

    implementa envio de token do usuario deletado para a collection tokens, no services users remove

--- src/services/documents/users/remove.js
+++ src/services/documents/users/remove.js
@@ -1,8 +1,9 @@
-const { USERS } = require('../../../utils/strings');
+const { USERS, TOKENS, ADMIN } = require('../../../utils/strings');
 const { remove, searchById } = require('../../../models')(USERS);
+const { create } = require('../../../models')(TOKENS);
 const { ATTRIBUTE_DISABLED } = require('../../../utils/magicNumbers');
 
-module.exports = async (id) => {
+module.exports = async (id, role, token) => {
   const user = await searchById(id, { password: ATTRIBUTE_DISABLED });
 
   if (!user) {
@@ -11,5 +12,11 @@ module.exports = async (id) => {
 
   await remove(id);
 
+  if (role !== ADMIN) {
+    await create({ token });
+  }
+
   return user;
 };
+
+// create({ token }) envia o token do usuário para blackList. vide services/auth/verifyToken.  
