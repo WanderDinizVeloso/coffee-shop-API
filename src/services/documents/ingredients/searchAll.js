f768ee3f613fd59954c1d6a7329cee67e0e11354
WanderDinizVeloso <wanveloso@yahoo.com.br>
2022-02-19 17:49:59 -0300

    cria services ingredients searchAll

---
+++ src/services/documents/ingredients/searchAll.js
@@ -0,0 +1,8 @@
+const { INGREDIENTS } = require('../../../utils/strings');
+const { search } = require('../../../models')(INGREDIENTS);
+
+module.exports = async () => {
+  const ingredients = await search({});
+
+  return ingredients;
+};
