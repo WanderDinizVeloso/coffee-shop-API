9e5a8b5aa8aa24947729764cc9b7a44287e3d780
WanderDinizVeloso <wanveloso@yahoo.com.br>
2022-02-19 20:21:55 -0300

    corrige atribuicao do name no services functions CheckNewNameOnDatabase

--- src/services/functions/checkNewNameOnDatabase.js
+++ src/services/functions/checkNewNameOnDatabase.js
@@ -2,7 +2,7 @@ const { filterField } = require('../../utils/pipelines');
 
 module.exports = async (newName, nameData, search) => {
   if (newName !== nameData) {
-    const verifiedName = (await search(filterField({ name: nameData })))[0];
+    const verifiedName = (await search(filterField({ name: newName })))[0];
 
     return verifiedName;
   }
