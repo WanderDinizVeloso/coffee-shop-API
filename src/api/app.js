08b64d6388c57b8345f0d68992e73dffa6dbd262
WanderDinizVeloso <wanveloso@yahoo.com.br>
2022-02-18 22:11:27 -0300

    implementa root e middleware error no app

--- src/api/app.js
+++ src/api/app.js
@@ -4,9 +4,14 @@ const express = require('express');
 const { json } = require('body-parser');
 const cors = require('cors');
 
+const root = require('../controllers/routers/root');
+const { error } = require('../controllers/middlewares');
+
 const app = express();
 
 app.use(cors());
 app.use(json());
+app.use(root);
+app.use(error);
 
 module.exports = app;
