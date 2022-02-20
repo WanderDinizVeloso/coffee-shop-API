353624697b2ba741884f906455bb37b486dc9e2f
WanderDinizVeloso <wanveloso@yahoo.com.br>
2022-02-20 14:38:32 -0300

    refatora implementacao do services products upload

--- src/services/documents/products/upload.js
+++ src/services/documents/products/upload.js
@@ -4,16 +4,14 @@ const { PRODUCTS } = require('../../../utils/strings');
 const { search, update } = require('../../../models')(PRODUCTS);
 const { filterField } = require('../../../utils/pipelines');
 
-module.exports = async ({ id: _id, image, originalUrl }) => {
+module.exports = async ({ id: _id, url }) => {
   const product = (await search(filterField({ _id: ObjectId(_id) })))[0];
 
   if (!product) {
     return null;
   }
-
-  const imageDestination = `${originalUrl}/${image}`;
   
-  await update({ ...product, image: imageDestination });
+  await update({ ...product, image: url });
 
   const newProductData = (await search(filterField({ _id: ObjectId(_id) })))[0];
 
