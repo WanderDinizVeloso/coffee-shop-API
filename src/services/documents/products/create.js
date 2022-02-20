3239ec60d1bef05a9c81f3488f35de0395930583
WanderDinizVeloso <wanveloso@yahoo.com.br>
2022-02-20 07:17:52 -0300

    corrige erro de digitacao nas variavel do services products create

--- src/services/documents/products/create.js
+++ src/services/documents/products/create.js
@@ -7,9 +7,9 @@ const { filterField } = require('../../../utils/pipelines');
 const { DECIMAL_PLACES_PRICE } = require('../../../utils/magicNumbers');
 
 module.exports = async ({ name, price }) => {
-  const productsExistsOnDatabase = (await search(filterField({ name })))[0];
+  const productExistsOnDatabase = (await search(filterField({ name })))[0];
 
-  if (productsExistsOnDatabase) {
+  if (productExistsOnDatabase) {
     return null;
   }
 
@@ -18,9 +18,9 @@ module.exports = async ({ name, price }) => {
     price: setDecimalPlaces(price, DECIMAL_PLACES_PRICE),
   });
 
-  const newProducts = (
+  const newProduct = (
     await search(filterField({ _id: ObjectId(insertedId) }))
   )[0];
 
-  return newProducts;
+  return newProduct;
 };
