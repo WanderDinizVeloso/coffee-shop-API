b4b07bb6e25e7131e9dfa224bb531002545e174f
WanderDinizVeloso <wanveloso@yahoo.com.br>
2022-02-20 12:44:41 -0300

    cria middleware upload

---
+++ src/controllers/middlewares/upload/upload.js
@@ -0,0 +1,47 @@
+const multer = require('multer');
+const path = require('path');
+
+const { STORAGE_TYPE } = process.env;
+
+const destination = path.resolve(__dirname, '../../../../temp/images');
+
+const { renameImageName } = require('../../../services/functions');
+const { invalidImageType } = require('../../statusAndMessage');
+const { IMAGE } = require('../../../utils/strings');
+const { imageTypeList } = require('../../../utils/lists');
+const { IMAGE_MAX_SIZE } = require('../../../utils/magicNumbers');
+
+const storageTypes = {
+  local: multer.diskStorage({
+    destination: (_req, _file, callback) => {
+      callback(null, destination);
+    },
+    filename: (req, file, callback) => {
+      const { id } = req.params;
+  
+      const newImageName = renameImageName(id, file);
+  
+      req.imageName = newImageName;
+  
+      callback(null, newImageName);
+    },
+  }),
+};
+
+module.exports = async (_req, _res, next) => multer({
+  dest: destination,
+  storage: storageTypes[STORAGE_TYPE],
+  limits: {
+    fileSize: IMAGE_MAX_SIZE,
+  },
+  fileFilter: (_reqFile, file, callback) => {
+    const allowedMimes = imageTypeList;
+
+    if (allowedMimes.includes(file.mimetype)) {
+      callback(null, true);
+      return next();
+    } 
+    
+    return next(invalidImageType(IMAGE));
+  },
+});
