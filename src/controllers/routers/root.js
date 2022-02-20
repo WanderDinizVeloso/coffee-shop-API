2aff5ba00e83f925e093b413d3956b2393ffba4f
WanderDinizVeloso <wanveloso@yahoo.com.br>
2022-02-20 14:39:30 -0300

    corrige caminho do products no root

--- src/controllers/routers/root.js
+++ src/controllers/routers/root.js
@@ -3,7 +3,7 @@ const express = require('express');
 const users = require('./users');
 const login = require('./login');
 const ingredients = require('./ingredients');
-const products = require('./ingredients');
+const products = require('./products');
 
 const root = express.Router({ mergeParams: true });
 
