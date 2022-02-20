908f1399eb83834488a873cc71644a18b1a785fe
WanderDinizVeloso <wanveloso@yahoo.com.br>
2022-02-20 14:36:38 -0300

    corrige implementacao do renameImageName no services functions

--- src/services/functions/renameImageName.js
+++ src/services/functions/renameImageName.js
@@ -2,7 +2,8 @@ const { IMAGE_FORMAT_POSITION } = require('../../utils/magicNumbers');
 const { IMAGE_STRING_SEPARATOR } = require('../../utils/strings');
 
 module.exports = (id, file) => {
-  const imageFormat = (file.split(IMAGE_STRING_SEPARATOR))[IMAGE_FORMAT_POSITION];
+  const { originalname: originalName } = file;
+  const imageFormat = (originalName.split(IMAGE_STRING_SEPARATOR))[IMAGE_FORMAT_POSITION];
 
   const newImageName = `${id}.${imageFormat}`;
 
