49218b12fcb3dfd4adeff487e4b5c940de718ce2
WanderDinizVeloso <wanveloso@yahoo.com.br>
2022-02-20 23:44:35 -0300

    refatora services ingredients create, implementando as novas funçoes do models

--- src/services/documents/ingredients/create.js
+++ src/services/documents/ingredients/create.js
@@ -1,15 +1,12 @@
-const { ObjectId } = require('mongodb');
-
 const { INGREDIENTS } = require('../../../utils/strings');
-const { search, create } = require('../../../models')(INGREDIENTS);
+const { searchByField, searchById, create } = require('../../../models')(INGREDIENTS);
 const { setDecimalPlaces } = require('../../functions');
-const { filterField } = require('../../../utils/pipelines');
 const {
   DECIMAL_PLACES_PRICE, DECIMAL_PLACES_QUANTITY, INITIAL_QUANTITY,
 } = require('../../../utils/magicNumbers');
 
 module.exports = async ({ name, unity, price }) => {
-  const ingredientExistsOnDatabase = (await search(filterField({ name })))[0];
+  const ingredientExistsOnDatabase = await searchByField({ name });
 
   if (ingredientExistsOnDatabase) {
     return null;
@@ -22,9 +19,7 @@ module.exports = async ({ name, unity, price }) => {
     price: setDecimalPlaces(price, DECIMAL_PLACES_PRICE),
   });
 
-  const newIngredient = (
-    await search(filterField({ _id: ObjectId(insertedId) }))
-  )[0];
+  const newIngredient = await searchById(insertedId);
 
   return newIngredient;
 };
