edd074283e31496eecc7b25cd92d785a0fb25282
WanderDinizVeloso <wanveloso@yahoo.com.br>
2022-02-20 23:18:20 -0300

    implementa searchAll, searchByField e searchById no index do models

--- src/models/index.js
+++ src/models/index.js
@@ -1,11 +1,15 @@
 const create = require('./documents/create');
 const remove = require('./documents/remove');
-const search = require('./documents/search');
+const searchAll = require('./documents/searchAll');
+const searchById = require('./documents/searchById');
+const searchByField = require('./documents/searchByField');
 const update = require('./documents/update');
 
 module.exports = (collection) => ({
   create: async (doc) => create(collection, doc),
   remove: async (id) => remove(collection, id),
-  search: async (pipeline) => search(collection, pipeline),
+  searchAll: async (filter, sort) => searchAll(collection, filter, sort),
+  searchById: async (id) => searchById(collection, id),
+  searchByField: async (field) => searchByField(collection, field),
   update: async (doc) => update(collection, doc),
 });
