3a314e4c4c1655db901db6e1bc7e22da83d00dd5
WanderDinizVeloso <wanveloso@yahoo.com.br>
2022-02-21 15:13:43 -0300

    implementa opcao de gerar o custo do produto no services products searchAll

--- src/services/documents/products/searchAll.js
+++ src/services/documents/products/searchAll.js
@@ -1,12 +1,18 @@
 const { PRODUCTS } = require('../../../utils/strings');
 const { searchAll } = require('../../../models')(PRODUCTS);
-const { generateIngredientNameList } = require('../../functions');
+const { generateIngredientNameList, generateProductCost } = require('../../functions');
 
-module.exports = async () => {
+module.exports = async (cost = false) => {
   const products = await searchAll();
 
-  const productsWithIngredientList = products.map((product) => generateIngredientNameList(product));
+  if (cost) {
+    const productsWithCost = products.map((product) => generateProductCost(product));
+    const productsWithCostResolve = await Promise.all(productsWithCost);
+  
+    return productsWithCostResolve;
+  }
 
+  const productsWithIngredientList = products.map((product) => generateIngredientNameList(product));
   const productsWithIngredientListResolve = await Promise.all(productsWithIngredientList);
 
   return productsWithIngredientListResolve;
