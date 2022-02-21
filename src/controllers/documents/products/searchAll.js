73540c42a1746cb3cc16b35b955f87432e547e17
WanderDinizVeloso <wanveloso@yahoo.com.br>
2022-02-21 15:35:25 -0300

    corrige forma de forma de passar o parametro cost do controllers products searchAll

--- src/controllers/documents/products/searchAll.js
+++ src/controllers/documents/products/searchAll.js
@@ -5,7 +5,7 @@ const { searchAll } = require('../../../services/documents/products');
 module.exports = async (req, res, _next) => {
   const { cost } = req.query;
 
-  const products = await searchAll();
+  const products = await searchAll({ cost });
 
   return res
     .status(OK)
