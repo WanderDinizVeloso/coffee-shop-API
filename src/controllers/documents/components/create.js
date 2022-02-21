3d544bbf6b85527a2f3d71cef6571420a87a80bc
WanderDinizVeloso <wanveloso@yahoo.com.br>
2022-02-20 22:16:50 -0300

    cria controllers components create

---
+++ src/controllers/documents/components/create.js
@@ -0,0 +1,22 @@
+const { CREATED } = require('http-status-codes').StatusCodes;
+
+const { create } = require('../../../services/documents/components');
+const { createdSuccessfully, registered } = require('../../statusAndMessage');
+const { COMPONENT } = require('../../../utils/strings');
+
+module.exports = async (req, res, next) => {
+  const { productId, ingredients } = req.body;
+
+  const created = await create({ productId, ingredients });
+
+  if (!created) {
+    return next(registered(COMPONENT));
+  }
+
+  return res
+    .status(CREATED)
+    .json({
+      message: createdSuccessfully(COMPONENT),
+      createdComponent: created,
+    });
+};
