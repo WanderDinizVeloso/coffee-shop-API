6fe8947d2f9fb9064d2b94a221a6b6b8ec94ab20
WanderDinizVeloso <wanveloso@yahoo.com.br>
2022-02-21 00:36:05 -0300

    refatora checkNewEmail, implementando as novas funçoes do models

--- src/services/functions/checkNewEmailOnDatabase.js
+++ src/services/functions/checkNewEmailOnDatabase.js
@@ -1,10 +1,9 @@
 const { USERS } = require('../../utils/strings');
-const { search } = require('../../models')(USERS);
-const { filterField } = require('../../utils/pipelines');
+const { searchByField } = require('../../models')(USERS);
 
 module.exports = async (newEmail, emailData) => {
   if (newEmail !== emailData) {
-    const verifiedEmail = (await search(filterField({ email: newEmail })))[0];
+    const verifiedEmail = await searchByField({ email: newEmail });
 
     return verifiedEmail;
   }
