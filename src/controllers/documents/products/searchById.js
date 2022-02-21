16bf6ce252a513356237d413e522b1c3dcf130f1
WanderDinizVeloso <wanveloso@yahoo.com.br>
2022-02-21 15:19:16 -0300

    implementa opcao de gerar o custo do produto no controllers products searchById

--- src/controllers/documents/products/searchById.js
+++ src/controllers/documents/products/searchById.js
@@ -6,8 +6,9 @@ const { PRODUCT } = require('../../../utils/strings');
 
 module.exports = async (req, res, next) => {
   const { id } = req.params;
+  const { cost } = req.query;
 
-  const product = await searchById({ id });
+  const product = await searchById({ id, cost });
 
   if (!product) {
     return next(notFound(PRODUCT));
