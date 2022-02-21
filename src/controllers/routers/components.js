3c16520d2c6195c1409c9b50592350a0a9532efd
WanderDinizVeloso <wanveloso@yahoo.com.br>
2022-02-20 22:28:14 -0300

    cria router components

---
+++ src/controllers/routers/components.js
@@ -0,0 +1,36 @@
+const express = require('express');
+
+const { create, remove, searchAll, searchById, update } = require('../documents/components');
+const { wrapper, authentication, admAuthorization } = require('../middlewares');
+
+const router = express.Router({ mergeParams: true });
+
+router.get('/', wrapper([
+  authentication,
+  searchAll,
+]));
+
+router.get('/:id', wrapper([
+  authentication,
+  searchById,
+]));
+
+router.post('/', wrapper([
+  authentication,
+  admAuthorization,
+  create,
+]));
+
+router.put('/:id', wrapper([
+  authentication,
+  admAuthorization,
+  update,
+]));
+
+router.delete('/:id', wrapper([
+  authentication,
+  admAuthorization,
+  remove,
+]));
+
+module.exports = router;
