f13122484b37da92c8fe2a81996da8a00f3b19c0
WanderDinizVeloso <wanveloso@yahoo.com.br>
2022-02-21 16:21:09 -0300

    adiciona parametro quantity no services ingredients update

--- src/services/documents/ingredients/update.js
+++ src/services/documents/ingredients/update.js
@@ -1,11 +1,9 @@
 const { INGREDIENTS, NAME_EXIST } = require('../../../utils/strings');
 const { searchById, searchByField, update } = require('../../../models')(INGREDIENTS);
 const { setDecimalPlaces, checkNewNameOnDatabase } = require('../../functions');
-const {
-  DECIMAL_PLACES_PRICE, DECIMAL_PLACES_QUANTITY, INITIAL_QUANTITY,
-} = require('../../../utils/magicNumbers');
+const { DECIMAL_PLACES_PRICE, DECIMAL_PLACES_QUANTITY } = require('../../../utils/magicNumbers');
 
-module.exports = async ({ id, name, unity, price }) => {
+module.exports = async ({ id, name, unity, quantity, price }) => {
   const ingredient = await searchById(id);
 
   if (!ingredient) {
@@ -22,7 +20,7 @@ module.exports = async ({ id, name, unity, price }) => {
     ...ingredient,
     name,
     unity,
-    quantity: setDecimalPlaces(INITIAL_QUANTITY, DECIMAL_PLACES_QUANTITY),
+    quantity: setDecimalPlaces(quantity, DECIMAL_PLACES_QUANTITY),
     price: setDecimalPlaces(price, DECIMAL_PLACES_PRICE),
   });
 
