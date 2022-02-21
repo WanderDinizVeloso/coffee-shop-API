58ecf8018956ce2cf849572e0207416a8e0e24a6
WanderDinizVeloso <wanveloso@yahoo.com.br>
2022-02-21 11:07:49 -0300

    altera implementacao do generateIngredientNameList para cobrir cados de produtos sem ingredients

--- src/services/functions/generateIngredientNameList.js
+++ src/services/functions/generateIngredientNameList.js
@@ -3,10 +3,10 @@ const { ObjectId } = require('mongodb');
 const { COMPONENTS } = require('../../utils/strings');
 const { searchByField } = require('../../models')(COMPONENTS);
 
-module.exports = async (product) => {
+module.exports = async (product = {}) => {
   const { _id: id } = product;
   
-  const { ingredients } = await searchByField({ productId: ObjectId(id).toString() });
+  const { ingredients = [] } = await searchByField({ productId: ObjectId(id).toString() }) || {};
   
   const ingredientsName = ingredients.map(({ name }) => name);
   
