2988e774fbdfbf62fc1c5c3ba249c315f751453b
WanderDinizVeloso <wanveloso@yahoo.com.br>
2022-02-22 17:26:49 -0300

    altera validateIngredients, visando implementar atributos opcionais no router ingredients update

--- src/controllers/middlewares/validations/validateIngredients.js
+++ src/controllers/middlewares/validations/validateIngredients.js
@@ -2,6 +2,7 @@ const Joi = require('@hapi/joi');
 Joi.objectId = require('joi-objectid')(Joi);
 
 const { schemas } = require('../../../utils/validationsSchema');
+const { PUT } = require('../../../utils/strings');
 
 const schema = Joi.object({
   name: schemas.nameRequired,
@@ -9,9 +10,19 @@ const schema = Joi.object({
   price: schemas.valuesRequired,
 });
 
+const schemaUpdate = Joi.object({
+  name: schemas.name,
+  unity: schemas.unitary,
+  price: schemas.values,
+});
+
 module.exports = async (req, _res, next) => {
   try {
-    await schema.validateAsync(req.body);
+    if (req.method === PUT) {
+      await schemaUpdate.validateAsync(req.body);
+    } else {
+      await schema.validateAsync(req.body);
+    }
   } catch (err) {
     next(err);
   }
