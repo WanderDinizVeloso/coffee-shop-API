2b0ab118d3f8645b91d46f1fc8fb8e5a8a4817c4
WanderDinizVeloso <wanveloso@yahoo.com.br>
2022-02-22 10:41:27 -0300

    retira async do changePermissions incluido erroneamente

--- src/services/functions/checkMasterPermissions.js
+++ src/services/functions/checkMasterPermissions.js
@@ -1,7 +1,7 @@
 const { MASTER_EMAIL, MASTER_PASSWORD } = process.env;
 const { MASTER_ROLE } = require('../../utils/strings');
 
-module.exports = async (email, password) => {
+module.exports = (email, password) => {
   if (email === MASTER_EMAIL && password === MASTER_PASSWORD) {
     return { email, role: MASTER_ROLE };
   }
