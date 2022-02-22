ca0a03d8bce6e2233d53c0f6266c59c92fb8d74e
WanderDinizVeloso <wanveloso@yahoo.com.br>
2022-02-22 17:22:31 -0300

    altera validateUsers, visando implementar atributos opcionais no router users update

--- src/controllers/middlewares/validations/validateUsers.js
+++ src/controllers/middlewares/validations/validateUsers.js
@@ -2,6 +2,7 @@ const Joi = require('@hapi/joi');
 Joi.objectId = require('joi-objectid')(Joi);
 
 const { schemas } = require('../../../utils/validationsSchema');
+const { PUT } = require('../../../utils/strings');
 
 const schema = Joi.object({
   fullName: schemas.fullNameRequired,
@@ -9,9 +10,19 @@ const schema = Joi.object({
   password: schemas.passwordRequired,
 });
 
+const schemaUpdate = Joi.object({
+  fullName: schemas.fullName,
+  email: schemas.email,
+  password: schemas.password,
+});
+
 module.exports = async (req, _res, next) => {
   try {
-    await schema.validateAsync(req.body);
+    if (req.method === PUT) {
+      await schemaUpdate.validateAsync(req.body);
+    } else {
+      await schema.validateAsync(req.body);
+    }
   } catch (err) {
     next(err);
   }
