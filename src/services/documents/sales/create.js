67b1da74f657663a1451167e70e4772c15d66143
WanderDinizVeloso <wanveloso@yahoo.com.br>
2022-02-22 01:46:55 -0300

    implementa stockUpdate no services sales create

--- src/services/documents/sales/create.js
+++ src/services/documents/sales/create.js
@@ -1,12 +1,19 @@
-const { SALES, USERS } = require('../../../utils/strings');
+const { SALES, PRODUCTS } = require('../../../utils/strings');
 const { searchById, create } = require('../../../models')(SALES);
-const { searchById: searchProduct } = require('../../../models')(USERS);
+const { searchById: searchProduct } = require('../../../models')(PRODUCTS);
+const { stockUpdate } = require('../../functions');
 
 module.exports = async ({ productId, quantity }) => {
-  const productExistsOnDatabase = await searchProduct({ id: productId });
+  const product = await searchProduct({ id: productId });
 
-  if (!productExistsOnDatabase) {
+  if (!product) {
     return null;
+  }
+
+  const { insufficientFunds, list } = await stockUpdate({ productId, quantity });
+
+  if (insufficientFunds) {
+    return { insufficientFunds, list };
   }
 
   const { insertedId } = await create({ productId, quantity });
