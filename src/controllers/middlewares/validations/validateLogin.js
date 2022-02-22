1d11a2b5795748be27aff702b4dab9e8ff5091f2
WanderDinizVeloso <wanveloso@yahoo.com.br>
2022-02-22 16:05:13 -0300

    altera validateLogin, visando padronizacao dos schemas

--- src/controllers/middlewares/validations/validateLogin.js
+++ src/controllers/middlewares/validations/validateLogin.js
@@ -1,4 +1,12 @@
-const { loginSchema } = require('../../../utils/validationsSchema');
+const Joi = require('@hapi/joi');
+Joi.objectId = require('joi-objectid')(Joi);
+
+const { schemas } = require('../../../utils/validationsSchema');
+
+const loginSchema = Joi.object({
+  email: schemas.emailRequired,
+  password: schemas.passwordRequired,
+});
 
 module.exports = async (req, _res, next) => {
   try {
