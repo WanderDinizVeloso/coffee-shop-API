b514798bef124a387e99ad84475f8d907f0a4de4
WanderDinizVeloso <wanveloso@yahoo.com.br>
2022-02-22 17:28:10 -0300

    altera validateComponents, visando implementar atributos opcionais no router components update

--- src/controllers/middlewares/validations/validateComponents.js
+++ src/controllers/middlewares/validations/validateComponents.js
@@ -2,15 +2,25 @@ const Joi = require('@hapi/joi');
 Joi.objectId = require('joi-objectid')(Joi);
 
 const { schemas } = require('../../../utils/validationsSchema');
+const { PUT } = require('../../../utils/strings');
 
 const schema = Joi.object({
   productId: schemas.idRequired,
   ingredients: schemas.ingredientsRequired,
 });
 
+const schemaUpdate = Joi.object({
+  productId: schemas.id,
+  ingredients: schemas.ingredients,
+});
+
 module.exports = async (req, _res, next) => {
   try {
-    await schema.validateAsync(req.body);
+    if (req.method === PUT) {
+      await schemaUpdate.validateAsync(req.body);
+    } else {
+      await schema.validateAsync(req.body);
+    }
   } catch (err) {
     next(err);
   }
