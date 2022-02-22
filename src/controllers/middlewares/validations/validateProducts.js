f00739f600d85f2da4a813132010a85d3ba73ac7
WanderDinizVeloso <wanveloso@yahoo.com.br>
2022-02-22 17:25:16 -0300

    altera validateProducts, visando implementar atributos opcionais no router products update

--- src/controllers/middlewares/validations/validateProducts.js
+++ src/controllers/middlewares/validations/validateProducts.js
@@ -2,15 +2,25 @@ const Joi = require('@hapi/joi');
 Joi.objectId = require('joi-objectid')(Joi);
 
 const { schemas } = require('../../../utils/validationsSchema');
+const { PUT } = require('../../../utils/strings');
 
 const schema = Joi.object({
   name: schemas.nameRequired,
   price: schemas.valuesRequired,
 });
 
+const schemaUpdate = Joi.object({
+  name: schemas.name,
+  price: schemas.values,
+});
+
 module.exports = async (req, _res, next) => {
   try {
-    await schema.validateAsync(req.body);
+    if (req.method === PUT) {
+      await schemaUpdate.validateAsync(req.body);
+    } else {
+      await schema.validateAsync(req.body);
+    }
   } catch (err) {
     next(err);
   }
