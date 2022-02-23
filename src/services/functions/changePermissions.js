9629113efee7b84bade2ba1aed55b2e727c61ccd
WanderDinizVeloso <wanveloso@yahoo.com.br>
2022-02-22 23:59:13 -0300

    remove console.log commitado erroneamente

--- src/services/functions/changePermissions.js
+++ src/services/functions/changePermissions.js
@@ -1,8 +1,6 @@
 const { ADMIN, MASTER_ROLE } = require('../../utils/strings');
 
 module.exports = (userRole, masterRole) => {
-  console.log(masterRole === MASTER_ROLE);
-
   if (masterRole === MASTER_ROLE) return ADMIN;
 
   return userRole;
